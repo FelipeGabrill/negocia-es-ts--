def958cfa2287c3982639042fec00b96a1caa1e1
FelipeGabrill <felipegabrielodss@gmail.com>
2024-08-19 18:30:17 -0300

    Criando view da mensagem

---
+++ app/views/mensagem-view.ts
@@ -0,0 +1,19 @@
+export class MensagemView {
+    
+    private element: HTMLElement;
+    
+    constructor(seletor: string) {
+        this.element = document.querySelector(seletor);
+    }
+
+    templete(model: string): string {
+        return `
+            <p class="alert alert-info">${model}</p>
+        `
+    }
+
+    update(model: string): void {
+        const templete = this.templete(model);
+        this.element.innerHTML = templete;
+    }
+}
---
+++ dist/js/views/mensagem-view.js
@@ -0,0 +1,14 @@
+export class MensagemView {
+    constructor(seletor) {
+        this.element = document.querySelector(seletor);
+    }
+    templete(model) {
+        return `
+            <p class="alert alert-info">${model}</p>
+        `;
+    }
+    update(model) {
+        const templete = this.templete(model);
+        this.element.innerHTML = templete;
+    }
+}
